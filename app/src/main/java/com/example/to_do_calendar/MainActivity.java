ae1546615e1f3024d72da933be35a424a37a50d6
ivanessco <ivanesscob@gmail.com>
2024-04-23 23:21:28 +0300

    create layout

---
+++ app/src/main/java/com/example/to_do_calendar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.to_do_calendar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.time.Instant;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button exit_button = findViewById(R.id.exit_button);
+        Button go_button = findViewById(R.id.go_button);
+        Button show_button = findViewById(R.id.show_button);
+    }
+    public void ExitFun(View v){
+        finish();
+    }
+
+    public void Go(View v){
+
+    }
+}
---
+++ app/src/main/java/com/example/to_do_calendar/insert_information.java
@@ -0,0 +1,30 @@
+package com.example.to_do_calendar;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class insert_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_insert_information);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+    Button back_button = findViewById(R.id.back_button);
+    Button accept_button = findViewById(R.id.accept_button);
+
+
+}
